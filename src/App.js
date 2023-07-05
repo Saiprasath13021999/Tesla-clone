4346a14fa1a1122fd78a8c6fb1a20d2e6ca2596d
Saiprasath13021999 <saiprasath777@gmail.com>
2023-07-05 18:14:12 +0530

    setting model3 page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Model from "./components/Model.js";
 import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <div className="bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-3-Mobile-NA.jpg')] lg:bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-3-Desktop-NA.jpg')] h-screen bg-cover bg-center ">
       <Navbar />
+      <Model />
     </div>
   );
 }
