87ebe12d29a93c81f8650732114b4ad9627c2e01
Saiprasath13021999 <saiprasath777@gmail.com>
2023-07-05 17:36:45 +0530

    setted navigation bar in the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      <h1>Tesla Clone</h1>
+    <div className="bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-3-Mobile-NA.jpg')] lg:bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-3-Desktop-NA.jpg')] h-screen bg-cover bg-center ">
+      <Navbar />
     </div>
   );
 }
